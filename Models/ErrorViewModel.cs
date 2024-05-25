5732f9277978d19e6474adc549157fa18aa4c964
xjilio123 <joaquin200314@gmail.com>
2024-05-25 18:10:04 -0500

    Arreglo de errores

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SiliconValley.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
