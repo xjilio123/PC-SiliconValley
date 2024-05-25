095389bf23619e46cc845ac02277db9c85d471aa
xjilio123 <joaquin200314@gmail.com>
2024-05-25 18:23:10 -0500

    cambios

--- Program.cs
+++ Program.cs
@@ -17,15 +17,4 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
